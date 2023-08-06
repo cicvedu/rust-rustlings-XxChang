3aaef6de3935d19c60ec318ed9ec355d02cd6930
XxChang <xuxchang@hotmail.com>
2023-08-06 23:23:42 +0800

    minor

--- exercises/drive/build.rs
+++ exercises/drive/build.rs
@@ -1,5 +1,5 @@
 fn main() {
     let timestamp = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs();
     println!("cargo:rustc-env=TEST_FOO={}", timestamp);
-    println!("cargo:rustc-cfg=pass");
+    println!("cargo:rustc-cfg=feature=\"pass\"");
 }
